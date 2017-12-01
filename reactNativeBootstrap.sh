a9bcc5790d74c8f679c74065ee5f06247c424e23
jkdrangel <jkdrangel@hotmail.com>
2017-12-01 13:38:16 -0300

    Fix or stamtment

--- reactNativeBootstrap.sh
+++ reactNativeBootstrap.sh
@@ -22,12 +22,12 @@ echo "export default App;" >> App/index.js;
 echo "Deseja instalar o redux [Y/n]?"
 read p
 p=${p:-"y"}
-if [ "$p" == "y" || "$p" == "Y" ]; then
+if [ "$p" == "y" -o "$p" == "Y" ]; then
   yarn add redux react-redux redux-logger 
   echo "Deseja instalar o reduxsauce [Y/n]?"
     read p
     p=${p:-"y"}
-    if [ "$p" == "y" || "$p" == "Y" ]; then
+    if [ "$p" == "y" -o "$p" == "Y" ]; then
       yarn add reduxsauce seamless-immutable
     fi
 fi
@@ -35,28 +35,28 @@ fi
 echo "Deseja instalar o moment [Y/n]?"
 read p
 p=${p:-"y"}
-if [ "$p" == "y" || "$p" == "Y" ]; then
+if [ "$p" == "y" -o "$p" == "Y" ]; then
   yarn add moment
 fi
 
 echo "Deseja instalar o lodash [Y/n]?"
 read p
 p=${p:-"y"}
-if [ "$p" == "y" || "$p" == "Y" ]; then
+if [ "$p" == "y" -o "$p" == "Y" ]; then
   yarn add lodash
 fi
 
 echo "Deseja instalar o React Navigation [Y/n]?"
 read p
 p=${p:-"y"}
-if [ "$p" == "y" || "$p" == "Y" ]; then
+if [ "$p" == "y" -o "$p" == "Y" ]; then
   yarn add react-navigation
 fi
 
 echo "Deseja instalar o React Native Firebase [Y/n]?"
 read p
 p=${p:-"y"}
-if [ "$p" == "y" || "$p" == "Y" ]; then
+if [ "$p" == "y" -o "$p" == "Y" ]; then
   yarn add react-native-firebase
   react-native link react-native-firebase
   echo "Para concluir a instalação siga as instruções em: https://rnfirebase.io/docs/v3.0.*/installation/initial-setup"
@@ -65,7 +65,7 @@ fi
 echo "Deseja instalar o Realm [Y/n]?"
 read p
 p=${p:-"y"}
-if [ "$p" == "y" || "$p" == "Y" ]; then
+if [ "$p" == "y" -o "$p" == "Y" ]; then
   yarn add realm
   react-native link realm
   echo "Para concluir a instalação siga as instruções em: https://rnfirebase.io/docs/v3.0.*/installation/initial-setup"
