6ec2dda00edf5fe7c421c005a2611f61d4ace1f8
jkdrangel <jkdrangel@hotmail.com>
2017-12-22 15:37:26 -0300

    Close splash in home

--- templates/screens/HomeScreen/HomeScreen.js
+++ templates/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,18 @@
 // import liraries
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import SplashScreen from 'react-native-smart-splash-screen';
 import styles from './style';
 
 // create a component
 class HomeScreen extends Component {
+    componentDidMount() {
+        SplashScreen.close({
+            animationType: SplashScreen.animationType.scale,
+            duration: 850,
+            delay: 500
+        });
+    }
     render() {
         return (
             <View style={styles.container}>
